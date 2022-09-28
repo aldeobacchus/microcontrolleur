5621dfea8a4f060f1d4bf47a5774562cd9b46139
aldeobacchus <75472779+aldeobacchus@users.noreply.github.com>
2022-09-28 11:12:10 +0200

    Add files via upload

--- tp1/Principal.c
+++ tp1/Principal.c
@@ -3,7 +3,7 @@ int main ( void )
 {
 	
 	
-	RCC->APB2ENR |= (0x01 << 2) | (0x01 << 3) | (0x01 << 4) ;
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPBEN | RCC_APB2ENR_IOPCEN;
 
 		//PC10  LED
 		GPIOC->CRH &= ~(0xF << 8);
@@ -16,9 +16,9 @@ int main ( void )
 		while (1)
 		{
 			if (GPIOC -> IDR & (1<<8)){
-				 GPIOC -> ODR &= ~ (1<<10);
+				 GPIOC -> ODR |= (1<<10);
 			}else{
-					GPIOC -> ODR |= (1 << 10);
+					GPIOC -> ODR &= ~(1 << 10);
 			}
 		}
 }
